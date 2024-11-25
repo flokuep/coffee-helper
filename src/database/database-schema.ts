aeedd6746938df905379fcf3c9a375e6897519e0
Florian Ostertag <git@florian-ostertag.de>
2024-11-25 09:49:55 +0100

    add: created and updated columns for beans

--- src/database/database-schema.ts
+++ src/database/database-schema.ts
@@ -1,4 +1,4 @@
-import { serial, text, pgTable } from 'drizzle-orm/pg-core';
+import { serial, text, pgTable, timestamp } from 'drizzle-orm/pg-core';
 
 export const beans = pgTable('beans', {
   id: serial('id').primaryKey(),
@@ -6,6 +6,11 @@ export const beans = pgTable('beans', {
   manufacturer: text('manufacturer'),
   profile: text('profile'),
   notes: text('notes'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
 });
 
 export const databaseSchema = {
