57cb6593c2034e56acdad6822a6362ebc342dad9
Florian Ostertag <git@florian-ostertag.de>
2024-11-26 10:23:21 +0100

    add: helmet for security

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
 
   const config = new DocumentBuilder()
     .setTitle('Coffee helper')
     .setDescription('The coffee helper API description')
     .setVersion('1.0')
-    .addTag('coffee')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
