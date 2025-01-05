913264820959b243769feacc1e1b052f2acc470a
Florian Ostertag <git@florian-ostertag.de>
2025-01-05 15:47:57 +0100

    chore: fix csp

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -7,7 +7,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(helmet());
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'"],
+        },
+      },
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
