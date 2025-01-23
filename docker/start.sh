3c0ffd9f55e470a366a323ac84a3f4ff0ea7f5dd
Florian Ostertag <git@florian-ostertag.de>
2025-01-23 15:27:53 +0100

    chore: print more details for listing

--- docker/start.sh
+++ docker/start.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
-echo "Running migrations..."
+echo 'Running migrations...'
 cd /usr/src/app/drizzle
-ls .
-ls meta
+ls -lart .
+ls -lart meta
 npm run migrate  & PID=$!
-# Wait for migration to finish
 wait $PID
-echo "Start application..."
-node /usr/src/app/dist/src/main "$@" & PID=$!
 
+echo 'Start application...'
+node /usr/src/app/dist/src/main "$@" & PID=$!
 wait $PID
