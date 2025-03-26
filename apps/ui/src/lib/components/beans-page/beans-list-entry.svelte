3480a694023054505d17c6904d9250000e17dfe0
Florian Ostertag <git@florian-ostertag.de>
2025-03-26 21:13:47 +0100

    add: show extraction in bean overview

--- apps/ui/src/lib/components/beans-page/beans-list-entry.svelte
+++ apps/ui/src/lib/components/beans-page/beans-list-entry.svelte
@@ -6,11 +6,20 @@
 	}
 
 	let { bean }: Props = $props();
+
+	let extraction = $derived(
+		bean.extractions.toSorted((a, b) => Date.parse(b.createdAt) - Date.parse(a.createdAt)).at(0)
+	);
 </script>
 
 <li class="my-2 rounded-xl border-1 border-stone-300 bg-stone-50 p-5">
 	<a href="/beans/{bean.id}">
 		{bean.name} - {bean.manufacturer}<br />
-		{bean.profile}
+		{#if extraction}
+			{#if extraction.flow !== 'perfect' || extraction.profile !== 'perfect'}
+				(!)
+			{/if}
+			{extraction.in}g &middot; {extraction.grind} &middot; {extraction.out}g &middot; {extraction.time}s
+		{/if}
 	</a>
 </li>
