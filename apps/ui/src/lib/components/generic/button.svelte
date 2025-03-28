f8b692570e3be83c5323f09d036dc7e0ce661787
Florian Ostertag <git@florian-ostertag.de>
2025-03-28 15:15:06 +0100

    chore: improve button layout

--- apps/ui/src/lib/components/generic/button.svelte
+++ apps/ui/src/lib/components/generic/button.svelte
@@ -19,5 +19,11 @@
 {#if type && type === 'link'}
 	<a {href} aria-label={description}>{label}</a>
 {:else}
-	<button {onclick} {type} aria-label={description} aria-roledescription={type}>{label}</button>
+	<button
+		class="my-1 w-full cursor-pointer rounded-md bg-yellow-600 px-2 py-2 text-sm text-yellow-50 hover:bg-yellow-900 hover:text-stone-200"
+		{onclick}
+		{type}
+		aria-label={description}
+		aria-roledescription={type}>{label}</button
+	>
 {/if}
