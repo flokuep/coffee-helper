57fe2490bb57dd7b8bbb7179763d079f86a32292
Florian Ostertag <git@florian-ostertag.de>
2025-04-22 22:11:26 +0200

    feat: leave screen on when entering bean details

--- apps/ui/src/routes/beans/[id=id]/+page.svelte
+++ apps/ui/src/routes/beans/[id=id]/+page.svelte
@@ -1,10 +1,36 @@
 <script lang="ts">
+	import { afterNavigate, beforeNavigate } from '$app/navigation';
 	import BeanDetails from '$lib/components/beans-details-page/bean-details.svelte';
 	import AppShellHeader from '$lib/components/generic/app-shell-header.svelte';
 	import AppShell from '$lib/components/generic/app-shell.svelte';
 	import { t } from '$lib/i18n.js';
 
 	let { data } = $props();
+	let screenLock: WakeLockSentinel | null | undefined = undefined;
+
+	// Set screen lock on entering the page
+	afterNavigate(async ({}) => {
+		if (isScreenLockSupported()) {
+			try {
+				screenLock = await navigator.wakeLock.request('screen');
+			} catch (error) {
+				console.log(`Cannot lock screen: ${error}`);
+			}
+		}
+	});
+
+	// Remove screen lock when leaving the page
+	beforeNavigate(async () => {
+		if (screenLock) {
+			screenLock.release().then(() => {
+				screenLock = undefined;
+			});
+		}
+	});
+
+	function isScreenLockSupported(): boolean {
+		return 'wakeLock' in navigator;
+	}
 </script>
 
 <AppShell>
