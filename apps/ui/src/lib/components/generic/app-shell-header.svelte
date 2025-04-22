0a16fb594ddca0cb13c6987c5158bad984836364
Florian Ostertag <git@florian-ostertag.de>
2025-04-22 20:48:06 +0200

    fix: adjust header role

--- apps/ui/src/lib/components/generic/app-shell-header.svelte
+++ apps/ui/src/lib/components/generic/app-shell-header.svelte
@@ -13,7 +13,7 @@
 	const { title, action }: Props = $props();
 </script>
 
-<header class="flex flex-row border-b-2 border-yellow-600 px-5 pb-2">
+<header class="flex flex-row border-b-2 border-yellow-600 px-5 pb-2" role="heading" aria-level="0">
 	<h1 class="grow text-xl font-medium">{title}</h1>
 	{#if action}
 		<Button type="link" description={action.label} label={action.label} href={action.href}></Button>
