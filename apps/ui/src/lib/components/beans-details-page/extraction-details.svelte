e3aad3988671aca75b4141116fad532d0703c060
Florian Ostertag <git@florian-ostertag.de>
2025-04-22 21:59:38 +0200

    feat: adjust extraction details layout

--- apps/ui/src/lib/components/beans-details-page/extraction-details.svelte
+++ apps/ui/src/lib/components/beans-details-page/extraction-details.svelte
@@ -2,6 +2,7 @@
 	import { t } from '$lib/i18n';
 	import Time from 'svelte-time/Time.svelte';
 	import type { Extraction } from '../../../generated/fetch-client';
+	import { Settings, Scale, Coffee, Watch, Droplets, MessageCircleQuestion } from '@lucide/svelte';
 
 	interface Props {
 		extraction: Extraction;
@@ -14,16 +15,49 @@
 	{$t('extractions.last')}
 	<Time relative timestamp={extraction.createdAt}></Time>
 </h1>
-<p class="pt-2">
-	{$t('extractions.in')}: {extraction.in}g &middot; {$t('extractions.grind')}: {extraction.grind}<br
-	/>
-	{$t('extractions.out')}: {extraction.out}g &middot; {$t('extractions.time')}: {extraction.time}s<br
-	/>
-	{$t('extractions.flow')}: {$t(`extractions.flowValues.${extraction.flow}`)} &middot; {$t(
-		'extractions.profile'
-	)}: {$t(`extractions.profileValues.${extraction.profile}`)}
+<div class="grid grid-cols-2 pt-2 md:grid-cols-[1fr_auto_auto_1fr]">
+	<div class="my-2 flex items-center justify-center p-1 md:col-start-2 md:p-5">
+		<Settings size={64} strokeWidth={1} class="mr-1" />
+		<span class="text-2xl">{extraction.grind}</span>
+	</div>
+
+	<div
+		class="my-2 flex items-center justify-center border-yellow-600 p-1 md:col-start-3 md:border-l-1 md:p-5"
+	>
+		<Scale size={64} strokeWidth={1} class="mr-2" />
+		<span class="text-2xl">{extraction.in}g</span>
+	</div>
+
+	<div class="col-span-2 my-4 border-b-1 border-yellow-600 md:col-start-2"></div>
+
+	<div class="my-2 flex items-center justify-center p-1 md:col-start-2 md:p-5">
+		<Watch size={64} strokeWidth={1} />
+		<span class="text-2xl">{extraction.time}s</span>
+	</div>
+
+	<div
+		class="my-2 flex items-center justify-center p-1 md:col-start-3 md:border-l-1 md:border-yellow-600 md:p-5"
+	>
+		<Coffee size={64} strokeWidth={1} class="mr-2" />
+		<span class="text-2xl">{extraction.out}g</span>
+	</div>
+
+	<div class="col-span-2 my-4 border-b-1 border-yellow-600 md:col-start-2"></div>
+
+	<div class="my-2 flex items-center justify-center p-1 md:col-start-2 md:p-5">
+		<Droplets size={64} strokeWidth={1} class="mr-1" />
+		<span class="text-2xl">{$t(`extractions.flowValues.${extraction.flow}`)}</span>
+	</div>
+
+	<div
+		class="my-2 flex items-center justify-center p-1 md:col-start-3 md:border-l-1 md:border-yellow-600 md:p-5"
+	>
+		<MessageCircleQuestion size={64} strokeWidth={1} class="mr-1" />
+		<span class="text-2xl">{$t(`extractions.profileValues.${extraction.profile}`)}</span>
+	</div>
+
 	{#if extraction.nextExtractionHint}
 		<br />
 		{extraction.nextExtractionHint}
 	{/if}
-</p>
+</div>
