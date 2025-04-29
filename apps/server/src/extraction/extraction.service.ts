6ad346ea0ff64cddc90d0097f4ee0e4e519ef213
Florian Ostertag <git@florian-ostertag.de>
2025-04-29 19:59:55 +0200

    chore: upgrade drizzle-orm

--- apps/server/src/extraction/extraction.service.ts
+++ apps/server/src/extraction/extraction.service.ts
@@ -1,7 +1,11 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateExtractionDto } from './dto/create-extraction.dto';
 import { UpdateExtractionDto } from './dto/update-extraction.dto';
-import { databaseSchema } from 'src/database/database-schema';
+import {
+  databaseSchema,
+  extractionFlow,
+  extractionProfile,
+} from 'src/database/database-schema';
 import { DrizzleService } from 'src/database/drizzle.service';
 import { eq } from 'drizzle-orm';
 import { Extraction } from './entities/extraction.entity';
@@ -14,9 +18,16 @@ export class ExtractionService {
     beanId: number,
     createExtractionDto: CreateExtractionDto,
   ): Promise<Extraction> {
+    const profile = extractionProfile.enumValues.find(
+      (val) => val === createExtractionDto.profile,
+    );
+    const flow = extractionFlow.enumValues.find(
+      (val) => val === createExtractionDto.flow,
+    );
+
     const createdExtraction = await this.drizzleService.db
       .insert(databaseSchema.extractions)
-      .values({ beanId, ...createExtractionDto })
+      .values({ beanId, ...createExtractionDto, profile: profile, flow: flow })
       .returning();
 
     return createdExtraction.pop();
@@ -46,9 +57,15 @@ export class ExtractionService {
     id: number,
     updateExtractionDto: UpdateExtractionDto,
   ): Promise<Extraction> {
+    const profile = extractionProfile.enumValues.find(
+      (val) => val === updateExtractionDto.profile,
+    );
+    const flow = extractionFlow.enumValues.find(
+      (val) => val === updateExtractionDto.flow,
+    );
     const updatedExtraction = await this.drizzleService.db
       .update(databaseSchema.extractions)
-      .set(updateExtractionDto)
+      .set({ ...updateExtractionDto, flow: flow, profile: profile })
       .where(eq(databaseSchema.extractions.id, id))
       .returning();
 
