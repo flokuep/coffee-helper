ab79caaa1a651e97033dc2889b0513142e09854f
Florian Ostertag <git@florian-ostertag.de>
2025-04-29 19:17:07 +0200

    chore: cleanup

--- apps/ui/src/lib/components/beans-details-page/extractions-table.svelte
+++ apps/ui/src/lib/components/beans-details-page/extractions-table.svelte
@@ -1,14 +1,6 @@
 <script lang="ts">
 	import { t } from '$lib/i18n';
-	import {
-		Coffee,
-		Droplets,
-		MessageCircleIcon,
-		MessageCircleQuestion,
-		Scale,
-		Settings,
-		Watch
-	} from '@lucide/svelte';
+	import { Coffee, Droplets, MessageCircleQuestion, Scale, Settings, Watch } from '@lucide/svelte';
 	import type { Extraction } from '../../../generated/fetch-client';
 	import ExtractionsListEntry from './extractions-table-entry.svelte';
 
@@ -20,12 +12,12 @@
 </script>
 
 {#if extractions.length > 0}
-	<h1 class="mt-5 mb-5 border-b-1 border-yellow-600">{$t('extractions.previous')}</h1>
-	<table class="w-full table-fixed">
+	<h1 class="mt-5 mb-2 border-b-1 border-yellow-600">{$t('extractions.previous')}</h1>
+	<table class="mx-2 w-full table-fixed border-separate border-spacing-y-2 text-sm">
 		<thead>
 			<tr>
 				<th><Settings strokeWidth={1} /></th>
-				<th> <Scale strokeWidth={1} /></th>
+				<th><Scale strokeWidth={1} /></th>
 				<th><Watch strokeWidth={1} /></th>
 				<th><Coffee strokeWidth={1} /></th>
 				<th><Droplets strokeWidth={1} /></th>
