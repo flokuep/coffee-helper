eb3b50783a9435177e375ad3bf0dee5d513d1045
Florian Ostertag <git@florian-ostertag.de>
2025-05-28 16:17:56 +0200

    chore: show icons on bean overview

--- apps/ui/src/lib/components/beans-page/beans-list-entry.svelte
+++ apps/ui/src/lib/components/beans-page/beans-list-entry.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { Coffee, Scale, Settings, ShieldAlert, Watch } from '@lucide/svelte';
 	import type { Bean } from '../../../generated/fetch-client';
 
 	interface Props {
@@ -14,12 +15,17 @@
 
 <li class="my-2 rounded-xl border-1 border-stone-300 bg-stone-50 p-5">
 	<a href="/beans/{bean.id}">
-		{bean.name}, {bean.manufacturer}<br />
+		<div>{bean.name}, {bean.manufacturer}</div>
 		{#if extraction}
-			{#if extraction.flow !== 'perfect' || extraction.profile !== 'perfect'}
-				(!)
-			{/if}
-			{extraction.in}g &middot; {extraction.grind} &middot; {extraction.out}g &middot; {extraction.time}s
+			<div class="flex flex-row items-center">
+				{#if extraction.flow !== 'perfect' || extraction.profile !== 'perfect'}
+					<ShieldAlert size={20} strokeWidth={2} fill="oklch(80.8% 0.114 19.571)" class="me-4" />
+				{/if}
+				<Scale size={20} strokeWidth={1} class="me-1" />{extraction.in}g
+				<Settings size={20} strokeWidth={1} class="ms-4 me-1" />{extraction.grind}
+				<Coffee size={20} strokeWidth={1} class="ms-4 me-1" />{extraction.out}g
+				<Watch size={20} strokeWidth={1} class="ms-3 me-0.5" />{extraction.time}s
+			</div>
 		{/if}
 	</a>
 </li>
